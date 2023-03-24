9acc38e3f65eb82b5c3cde3c73bd204cbb3fc6a9
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-03-24 09:22:00 -0500

    intro to java exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, world!");
+        System.out.print(" Welcome to the world of Java!");
+    }
+}
+
