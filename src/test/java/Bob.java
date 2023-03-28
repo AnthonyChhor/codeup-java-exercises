a3d4d977625d95380cacd54e92be35f5200dbe47
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-03-27 20:52:39 -0500

    strings exercises

---
+++ src/test/java/Bob.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner initiation = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hey I am BOB, would you like to have a conversation?");
+        String userChoice = initiation.nextLine();
+        if (userChoice.equalsIgnoreCase("Yes")) {
+            System.out.println("What would you like to talk about?");
+        }
+        while (userChoice.equalsIgnoreCase("yes")) {
+            String userInput = input.nextLine();
+
+
+            if (userChoice.equalsIgnoreCase("Yes")) {
+
+                if (userInput.endsWith("?")) {
+                    System.out.println("Sure.");
+                } else if (userInput.isEmpty()) {
+                    System.out.println("Fine. Be that way!");
+                } else if (userInput.endsWith("!")) {
+                    System.out.println("Whoa, chill out!");
+                } else if (userInput.equalsIgnoreCase("bye")) {
+                    break;
+                } else {
+                    System.out.println("Whatever.");
+                }
+            }
+
+        }
+        System.out.println("Okay. I will be here when you want to talk.");
+    }
+}
