325e27656fc9374f2d52d91859e95a86e1a33312
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-03-29 15:19:35 -0500

    persons exercise

---
+++ src/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    private String name;
+    public static void main(String[] args) {
+
+        Person person1 = new Person("John");
+        Person person2 = new Person("Jane");
+        person2.setName("Jane");
+        person1.sayHello();
+        person2.sayHello();
+
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void sayHello(){
+        System.out.printf("Hello, %s\n", this.name);
+    }
+
+}
