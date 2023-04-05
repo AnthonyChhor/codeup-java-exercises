f7ae28fae21646cae66238521982bf41f16ff1ac
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-04-05 09:04:41 -0500

    interfaces and abstract classes

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,17 +2,25 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 5);
+//        Rectangle box1 = new Rectangle(4, 5);
+//
+//        System.out.println(box1.getArea());
+//
+//        System.out.println(box1.getPerimeter());
+//
+//
+//        Rectangle box2 = new Square(5);
+//
+//        System.out.println(box2.getArea());
+//
+//        System.out.println(box2.getPerimeter());
 
-        System.out.println(box1.getArea());
+        Measurable myShapeOne = new Rectangle(5, 5);
+        System.out.println(myShapeOne.getArea());
+        System.out.println(myShapeOne.getPerimeter());
 
-        System.out.println(box1.getPerimeter());
-
-
-        Rectangle box2 = new Square(5);
-
-        System.out.println(box2.getArea());
-
-        System.out.println(box2.getPerimeter());
+        Measurable myShapeTwo = new Square(5);
+        System.out.println(myShapeTwo.getArea());
+        System.out.println(myShapeTwo.getPerimeter());
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,18 +1,44 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
+
+//    public Square(double side) {
+//        super(side, side);
+//    }
+//
+//    @Override
+//    public double getPerimeter() {
+//        return 4 * length;
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        return Math.pow(length, 2);
+//    }
+    protected double side;
 
     public Square(double side) {
         super(side, side);
+        this.side = side;
     }
 
     @Override
     public double getPerimeter() {
-        return 4 * length;
+        return 4 * side;
     }
 
     @Override
     public double getArea() {
-        return Math.pow(length, 2);
+        return Math.pow(side, 2);
+    }
+
+    @Override
+    public void setLength(double length) {
+        this.side = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.side = width;
     }
 }
